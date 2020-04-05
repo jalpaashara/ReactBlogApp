6c55f2c6050070e7eee34105e95b0e6937535a37
ashar <ashara.jalpa@gmail.com>
2020-04-04 21:32:25 -0400

    Updates

---
+++ precache-manifest.623f9f7930988b116a034c50df0509cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ed0a6cb631a62d82ba5292da2976de",
+    "url": "/ReactBlogApp/index.html"
+  },
+  {
+    "revision": "c823cc325c0433de8be5",
+    "url": "/ReactBlogApp/static/css/main.e2414e90.chunk.css"
+  },
+  {
+    "revision": "6ae4b30470235a221747",
+    "url": "/ReactBlogApp/static/js/2.65156925.chunk.js"
+  },
+  {
+    "revision": "c823cc325c0433de8be5",
+    "url": "/ReactBlogApp/static/js/main.38dbbee7.chunk.js"
+  },
+  {
+    "revision": "41fe8764cc36a80dc608",
+    "url": "/ReactBlogApp/static/js/runtime-main.64398928.js"
+  },
+  {
+    "revision": "a28ab548d958aa815f4c49a000e4230d",
+    "url": "/ReactBlogApp/static/media/aloo.a28ab548.jpg"
+  },
+  {
+    "revision": "a5f3bdc24adabae763efebe04f8a1e49",
+    "url": "/ReactBlogApp/static/media/chole.a5f3bdc2.jpg"
+  },
+  {
+    "revision": "1bb710bc3bf72cb1f52ce5aa879e7859",
+    "url": "/ReactBlogApp/static/media/idli.1bb710bc.jpg"
+  }
+]);
